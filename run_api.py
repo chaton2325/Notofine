0423f1037bc30e750b7803b80d1c442a29e80272
Tsari Sila <developpement0123@gmail.com>
2025-12-06 20:26:07 +0100

    super

--- run_api.py
+++ run_api.py
@@ -16,6 +16,5 @@ if __name__ == "__main__":
         "main:app",
         host="0.0.0.0",
         port=8080,
-        reload=True,  # Rechargement automatique en développement
         log_level="info"
     )
